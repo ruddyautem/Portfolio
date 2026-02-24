b6d6ab0ab651e10a7be29a776143def02299429b
ruddyautem <panderawan@gmail.com>
2026-02-24 20:19:37 +0100

    updated about page, clearer on both mobile devices and large monitors. More to come soon

--- src/components/TopPageDecoration/TopPageDecoration.jsx
+++ src/components/TopPageDecoration/TopPageDecoration.jsx
@@ -1,6 +1,6 @@
 const TopPageDecoration = ({ filename }) => {
   return (
-    <div className="flex h-12 items-center bg-gradient-to-r from-slate-800/50 to-slate-900/15 px-6">
+    <div className="flex h-12 items-center bg-linear-to-r from-slate-800/50 to-slate-900/15 px-6">
       <div className="flex items-center gap-2 font-mono text-xs text-slate-400">
         <span className="h-3 w-3 rounded-full bg-red-500/80"></span>
         <span className="h-3 w-3 rounded-full bg-yellow-500/80"></span>
